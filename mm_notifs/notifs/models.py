4dddc5e53e33e5557a01c83c927cc61b44745805
Ale <a.menchetti8@gmail.com>
2024-10-02 18:12:25 -0400

    Set up the database connection and created a simple model in models.py

--- mm_notifs/notifs/models.py
+++ mm_notifs/notifs/models.py
@@ -4,4 +4,11 @@ from django.db import models
 
 
 class Notification(models.Model):
-    pass
+    NotificationID = models.AutoField(primary_key=True)
+    Message = models.CharField(max_length=255)
+    SentStatus = models.BooleanField(default=False)
+    CreatedAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.Message
+
